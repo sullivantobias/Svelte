58992913e8534cabfa23519db3a67c508f4789a4
Tobias <stobias@sqli.com>
2019-05-20 15:12:04 +0200

    Init Svelte test

---
+++ svelte-test/src/App.svelte
@@ -0,0 +1,45 @@
+<script>
+  import Test from "./Test.svelte";
+  let count = 0;
+  let clicked = false;
+
+  $: double = count * 2;
+  $: triple = count * 3;
+  $: totalTest = [];
+
+  const handleClick = e => {
+    clicked = !clicked;
+    count++;
+    totalTest = [...totalTest, count];
+  };
+</script>
+
+<style>
+  button {
+    padding: 10px;
+  }
+
+  .clicked {
+    background-color: black;
+    color: white;
+  }
+
+  .item {
+    display: inline;
+    margin-right: 10px;
+  }
+</style>
+
+<button on:click|preventDefault={handleClick} class:clicked>Add Test</button>
+
+{#if totalTest.length}
+  <h1>Total</h1>
+  {#each totalTest as test}
+    <p class="item">{test}</p>
+  {/each}
+{/if}
+
+<Test test={count} />
+
+<p>Double is {double}</p>
+<p>Triple is {triple}</p>
---
+++ svelte-test/src/Test.svelte
@@ -0,0 +1,5 @@
+<script>
+  export let test = 0;
+</script>
+
+<p>{test > 0 ? `I'm the test number ${test}` : `No test yet`}</p>
